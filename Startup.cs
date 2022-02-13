9d53a6fb0048016a060f643b4023d4d8acb0c78f
M. Orawan <46880639+momijizen@users.noreply.github.com>
2022-02-13 20:56:39 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_net45.Startup))]
+namespace test_net45
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
